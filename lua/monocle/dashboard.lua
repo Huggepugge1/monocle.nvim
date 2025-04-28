ac9d8963d21a48aabca2412f30e6bece39889b16
Hugo Lindström <huggepugge1@gmail.com>
2025-04-28 16:31:05 +0200

    commit number stats to dashboard

--- lua/monocle/dashboard.lua
+++ lua/monocle/dashboard.lua
@@ -6,22 +6,6 @@ function M.init()
 	vim.api.nvim_create_user_command('MonocleDashboard', M.show_dashboard, {})
 end
 
-local function header(buf)
-	local top_line = '┌' .. string.rep('─', buf.width - 2) .. '┐'
-	local bottom_line = '└' .. string.rep('─', buf.width - 2) .. '┘'
-
-	local spaces = '│' .. string.rep(' ', buf.width - 2) .. '│'
-
-	local name = git.get_project_name()
-	local header_name = '│' ..
-		string.rep(' ', (buf.width - 2 - #name) / 2) ..
-		name ..
-		string.rep(' ', (buf.width - 2 - #name) / 2) ..
-		'│'
-
-	return { top_line, spaces, header_name, spaces, bottom_line }
-end
-
 function M.show_dashboard()
 	local buf = vim.api.nvim_create_buf(false, false)
 	local width = math.floor(vim.o.columns * 0.6)
@@ -37,12 +21,15 @@ function M.show_dashboard()
 		col = col,
 		style = 'minimal',
 		border = 'rounded',
+		title = git.get_project_name(),
+		title_pos = 'center',
 	}
 
 	local win = vim.api.nvim_open_win(buf, true, win_config)
 
 	local lines = {
-		unpack(header(win_config))
+		'Commits in ' .. git.get_branch_name() .. ': ' .. git.get_number_of_commits_in_branch(),
+		'Commits in repo: ' .. git.get_number_of_commits_in_repo(),
 	}
 
 	vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
